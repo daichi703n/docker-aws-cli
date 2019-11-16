94d79f202cf8f33f4e7cc9561e47d373dd983e60
Daichi Kimura <daichi703n@gmail.com>
2019-11-17 01:58:44 +0900

    CredHub Prevent overwrite

--- ci/credhub-set.sh
+++ ci/credhub-set.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -eu pipefail
 
 #AWS Credentials
@@ -6,15 +7,61 @@ AWS_DEFAULT_REGION=`cat ~/.aws/credentials | grep "aws_default_region" | awk -F'
 AWS_ACCESS_KEY_ID=`cat ~/.aws/credentials | grep "aws_access_key_id" | awk -F'aws_access_key_id=' '{print $2}'`
 AWS_SECRET_ACCESS_KEY=`cat ~/.aws/credentials | grep "aws_secret_access_key" | awk -F'aws_secret_access_key=' '{print $2}'`
 
-credhub set -t value -n /concourse/main/aws_default_region -v ${AWS_DEFAULT_REGION}
-credhub set -t value -n /concourse/main/aws_access_key_id -v ${AWS_ACCESS_KEY_ID}
-credhub set -t value -n /concourse/main/aws_secret_access_key -v ${AWS_SECRET_ACCESS_KEY}
+set +e
+credhub get -n /concourse/main/aws_default_region > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Skip: /concourse/main/aws_default_region exists."
+else
+  set -e
+  credhub set -t value -n /concourse/main/aws_default_region -v ${AWS_DEFAULT_REGION}
+fi
+
+set +e
+credhub get -n /concourse/main/aws_access_key_id > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Skip: /concourse/main/aws_access_key_id exists."
+else
+  set -e
+  credhub set -t value -n /concourse/main/aws_access_key_id -v ${AWS_ACCESS_KEY_ID}
+fi
+
+set +e
+credhub get -n /concourse/main/aws_secret_access_key > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Skip: /concourse/main/aws_secret_access_key exists."
+else
+  set -e
+  credhub set -t value -n /concourse/main/aws_secret_access_key -v ${AWS_SECRET_ACCESS_KEY}
+fi
 
 #Docker Credentials
-echo -n DOCKER_USERNAME:
-read DOCKER_USERNAME
-echo -n DOCKER_PASSWORD:
-read DOCKER_PASSWORD
+set +e
+credhub get -n /concourse/main/docker_username > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Skip: /concourse/main/docker_username exists."
+else
+  set -e
+  echo -n "DOCKER_USERNAME: "
+  read DOCKER_USERNAME
+  credhub set -t value -n /concourse/main/docker_username -v ${DOCKER_USERNAME}
+fi
 
-credhub set -t value -n /concourse/main/docker_username -v ${DOCKER_USERNAME}
-credhub set -t value -n /concourse/main/docker_password -v ${DOCKER_PASSWORD}
+set +e
+credhub get -n /concourse/main/docker_password > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Skip: /concourse/main/docker_password exists."
+else
+  set -e
+  echo -n "DOCKER_PASSWORD(secret): "
+  read -s DOCKER_PASSWORD
+  credhub set -t value -n /concourse/main/docker_password -v ${DOCKER_PASSWORD}
+fi
+
+set +e
+credhub get -n /concourse/main/github_private_key > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Skip: /concourse/main/github_private_key exists."
+else
+  set -e
+  credhub set -t value -n /concourse/main/github_private_key -v "$(cat ~/.ssh/keys/concourse/id_rsa)"
+fi
